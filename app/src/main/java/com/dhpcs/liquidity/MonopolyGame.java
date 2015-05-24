b2570ff34cbd8081dd271cc069fe47865abbe218
David Piggott <david@piggott.me.uk>
2015-05-24 21:00:14 +0100

    First pass adding MonopolyGame

---
+++ app/src/main/java/com/dhpcs/liquidity/MonopolyGame.java
@@ -0,0 +1,57 @@
+package com.dhpcs.liquidity;
+
+import android.content.Context;
+
+import com.dhpcs.liquidity.models.CreateZone;
+import com.dhpcs.liquidity.models.JoinZone;
+import com.dhpcs.liquidity.models.QuitZone;
+import com.dhpcs.liquidity.models.ZoneCreated;
+import com.dhpcs.liquidity.models.ZoneId;
+
+import de.greenrobot.event.EventBus;
+
+public class MonopolyGame {
+
+    private static final GameType ZONE_TYPE = GameType.MONOPOLY;
+
+    public static CreateZone getCreateZoneCommand(String name) {
+        return new CreateZone(name, ZONE_TYPE.typeName);
+    }
+
+    public static MonopolyGame initialize(
+            ServerConnection serverConnection,
+            Context context,
+            ZoneCreated zoneCreatedEvent) {
+        return new MonopolyGame(serverConnection, context, zoneCreatedEvent.zoneId());
+    }
+
+    public static MonopolyGame rejoin(
+            ServerConnection serverConnection,
+            Context context,
+            ZoneId zoneId) {
+        return new MonopolyGame(serverConnection, context, zoneId);
+    }
+
+    private final ServerConnection serverConnection;
+    private final ZoneStore zoneStore;
+    private final EventBus eventBus = EventBus.getDefault();
+    private final ZoneId zoneId;
+
+    private MonopolyGame(
+            ServerConnection serverConnection,
+            Context context,
+                         ZoneId zoneId) {
+        this.serverConnection = serverConnection;
+        this.zoneId = zoneId;
+        this.zoneStore = new ZoneStore(context, ZONE_TYPE.typeName, zoneId);
+    }
+
+    public void join() {
+//        serverConnection.execute(new JoinZone(zoneId));
+    }
+
+//    public void quit() {
+//        serverConnection.execute(new QuitZone(zoneId));
+//    }
+
+}
