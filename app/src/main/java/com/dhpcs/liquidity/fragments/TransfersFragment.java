0d74978eae2789a450e9648574854dac49c85af3
David Piggott <david@piggott.me.uk>
2015-07-22 13:01:54 +0100

    Convert TransfersFragment from ListView to RecyclerView

--- app/src/main/java/com/dhpcs/liquidity/fragments/TransfersFragment.java
+++ app/src/main/java/com/dhpcs/liquidity/fragments/TransfersFragment.java
@@ -3,12 +3,14 @@ package com.dhpcs.liquidity.fragments;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.util.SortedListAdapterCallback;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.dhpcs.liquidity.MonopolyGame.Player;
@@ -22,69 +24,134 @@ import java.util.List;
 
 import scala.collection.Iterator;
 
-// TODO: Extend ListFragment? Or switch to RecyclerView?
 public class TransfersFragment extends Fragment {
 
-    private static class TransfersAdapter extends ArrayAdapter<TransferWithCurrency> {
+    private class TransfersAdapter
+            extends RecyclerView.Adapter<TransfersAdapter.TransferViewHolder> {
 
-        private final DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        public class TransferViewHolder extends RecyclerView.ViewHolder {
+
+            private final TextView textViewSummary;
+            private final TextView textViewCreated;
+
+            public TransferViewHolder(View itemView) {
+                super(itemView);
+                textViewSummary = (TextView) itemView.findViewById(android.R.id.text1);
+                textViewSummary.setGravity(Gravity.START);
+                textViewCreated = (TextView) itemView.findViewById(android.R.id.text2);
+                textViewCreated.setGravity(Gravity.END);
+            }
+
+            public void bindTransfer(TransferWithCurrency transfer) {
+
+                long created = transfer.transaction().created();
+                boolean isFromPlayer = player != null && transfer.from().right().get().memberId()
+                        .equals(player.memberId());
+                String value = MonopolyGameActivity.formatCurrencyValue(
+                        context,
+                        transfer.currency(),
+                        transfer.transaction().value()
+                );
+                boolean isToPlayer = player != null && transfer.to().right().get().memberId()
+                        .equals(player.memberId());
+                String summary;
+                if (isFromPlayer && !isToPlayer) {
+                    summary = context.getString(
+                            R.string.transfer_summary_sent_to_format_string,
+                            value,
+                            MonopolyGameActivity.formatMemberOrAccount(context, transfer.to())
+                    );
+                } else if (!isFromPlayer && isToPlayer) {
+                    summary = context.getString(
+                            R.string.transfer_summary_received_from_format_string,
+                            value,
+                            MonopolyGameActivity.formatMemberOrAccount(context, transfer.from())
+                    );
+                } else {
+                    summary = context.getString(
+                            R.string.transfer_summary_format_string,
+                            MonopolyGameActivity.formatMemberOrAccount(context, transfer.from()),
+                            value,
+                            MonopolyGameActivity.formatMemberOrAccount(context, transfer.to())
+                    );
+                }
+
+                textViewCreated.setText(dateFormat.format(created));
+                textViewSummary.setText(summary);
+            }
+
+        }
+
+        private final Context context;
         private final Player player;
+        private final DateFormat dateFormat = DateFormat.getDateTimeInstance();
+        private final SortedList<TransferWithCurrency> transfers = new SortedList<>(
+                TransferWithCurrency.class,
+                new SortedListAdapterCallback<TransferWithCurrency>(this) {
+
+                    @Override
+                    public int compare(TransferWithCurrency o1,
+                                       TransferWithCurrency o2) {
+                        long lhsCreated = o1.transaction().created();
+                        long rhsCreated = o2.transaction().created();
+                        return -1 * (lhsCreated < rhsCreated ? -1 : (lhsCreated == rhsCreated ? 0 : 1));
+                    }
+
+                    @Override
+                    public boolean areContentsTheSame(TransferWithCurrency oldItem,
+                                                      TransferWithCurrency newItem) {
+                        return oldItem.equals(newItem);
+                    }
+
+                    @Override
+                    public boolean areItemsTheSame(TransferWithCurrency item1,
+                                                   TransferWithCurrency item2) {
+                        return item1.transactionId().equals(item2.transactionId());
+                    }
+
+                }
+        );
 
         public TransfersAdapter(Context context, Player player) {
-            super(context,
-                    // TODO
-                    android.R.layout.simple_list_item_2,
-                    android.R.id.text1);
+            this.context = context;
             this.player = player;
         }
 
+
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = super.getView(position, convertView, parent);
+        public int getItemCount() {
+            return transfers.size();
+        }
 
-            TransferWithCurrency transfer = getItem(position);
+        @Override
+        public TransferViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater
+                    .from(parent.getContext())
+                            // TODO
+                    .inflate(android.R.layout.simple_list_item_2, parent, false);
+            return new TransferViewHolder(view);
+        }
 
-            TextView textViewSummary = (TextView) view.findViewById(android.R.id.text1);
-            textViewSummary.setGravity(Gravity.START);
-            TextView textViewCreated = (TextView) view.findViewById(android.R.id.text2);
-            textViewCreated.setGravity(Gravity.END);
+        @Override
+        public void onBindViewHolder(TransferViewHolder holder, int position) {
+            TransferWithCurrency transfer = transfers.get(position);
+            holder.bindTransfer(transfer);
+        }
 
-            long created = transfer.transaction().created();
-            boolean isFromPlayer = player != null && transfer.from().right().get().memberId()
-                    .equals(player.memberId());
-            String value = MonopolyGameActivity.formatCurrencyValue(
-                    getContext(),
-                    transfer.currency(),
-                    transfer.transaction().value()
-            );
-            boolean isToPlayer = player != null && transfer.to().right().get().memberId()
-                    .equals(player.memberId());
-            String summary;
-            if (isFromPlayer && !isToPlayer) {
-                summary = getContext().getString(
-                        R.string.transfer_summary_sent_to_format_string,
-                        value,
-                        MonopolyGameActivity.formatMemberOrAccount(getContext(), transfer.to())
-                );
-            } else if (!isFromPlayer && isToPlayer) {
-                summary = getContext().getString(
-                        R.string.transfer_summary_received_from_format_string,
-                        value,
-                        MonopolyGameActivity.formatMemberOrAccount(getContext(), transfer.from())
-                );
-            } else {
-                summary = getContext().getString(
-                        R.string.transfer_summary_format_string,
-                        MonopolyGameActivity.formatMemberOrAccount(getContext(), transfer.from()),
-                        value,
-                        MonopolyGameActivity.formatMemberOrAccount(getContext(), transfer.to())
-                );
-            }
+        public int add(TransferWithCurrency transfer) {
+            return transfers.add(transfer);
+        }
 
-            textViewCreated.setText(dateFormat.format(created));
-            textViewSummary.setText(summary);
+        public void beginBatchedUpdates() {
+            transfers.beginBatchedUpdates();
+        }
 
-            return view;
+        public void clear() {
+            transfers.clear();
+        }
+
+        public void endBatchedUpdates() {
+            transfers.endBatchedUpdates();
         }
 
     }
@@ -102,7 +169,9 @@ public class TransfersFragment extends Fragment {
         return transfersFragment;
     }
 
-    private ArrayAdapter<TransferWithCurrency> arrayAdapter;
+    private TransfersAdapter transfersAdapter;
+
+    private TextView textViewEmpty;
 
     @Override
     @SuppressWarnings("unchecked")
@@ -113,18 +182,19 @@ public class TransfersFragment extends Fragment {
         List<TransferWithCurrency> transfers =
                 (ArrayList<TransferWithCurrency>) getArguments().getSerializable(ARG_TRANSFERS);
 
-        arrayAdapter = new TransfersAdapter(getActivity(), player);
+        transfersAdapter = new TransfersAdapter(getActivity(), player);
 
         if (transfers != null) {
+            transfersAdapter.beginBatchedUpdates();
             for (TransferWithCurrency transfer : transfers) {
                 if (player == null || (transfer.from().isRight()
                         && player.memberId().equals(transfer.from().right().get().memberId()))
                         || (transfer.to().isRight()
                         && player.memberId().equals(transfer.to().right().get().memberId()))) {
-                    arrayAdapter.add(transfer);
+                    transfersAdapter.add(transfer);
                 }
             }
-            arrayAdapter.sort(MonopolyGameActivity.transferComparator);
+            transfersAdapter.endBatchedUpdates();
         }
 
     }
@@ -135,17 +205,23 @@ public class TransfersFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transfers_list, container, false);
 
-        AbsListView absListViewPlayers = (AbsListView) view.findViewById(android.R.id.list);
-        absListViewPlayers.setAdapter(arrayAdapter);
-        absListViewPlayers.setEmptyView(view.findViewById(android.R.id.empty));
+        textViewEmpty = (TextView) view.findViewById(R.id.textview_empty);
+        textViewEmpty.setVisibility(
+                transfersAdapter.getItemCount() == 0 ? View.VISIBLE : View.GONE
+        );
+
+        RecyclerView recyclerViewTransfers = (RecyclerView) view.findViewById(R.id.recyclerview);
+        recyclerViewTransfers.setHasFixedSize(true);
+        recyclerViewTransfers.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerViewTransfers.setAdapter(transfersAdapter);
 
         return view;
     }
 
     public void onTransfersChanged(scala.collection.Iterable<TransferWithCurrency> transfers) {
         Player player = (Player) getArguments().getSerializable(ARG_PLAYER);
-        arrayAdapter.setNotifyOnChange(false);
-        arrayAdapter.clear();
+        transfersAdapter.beginBatchedUpdates();
+        transfersAdapter.clear();
         Iterator<TransferWithCurrency> iterator = transfers.iterator();
         while (iterator.hasNext()) {
             TransferWithCurrency transfer = iterator.next();
@@ -153,11 +229,13 @@ public class TransfersFragment extends Fragment {
                     && player.memberId().equals(transfer.from().right().get().memberId()))
                     || (transfer.to().isRight()
                     && player.memberId().equals(transfer.to().right().get().memberId()))) {
-                arrayAdapter.add(transfer);
+                transfersAdapter.add(transfer);
             }
         }
-        arrayAdapter.sort(MonopolyGameActivity.transferComparator);
-        arrayAdapter.notifyDataSetChanged();
+        textViewEmpty.setVisibility(
+                transfersAdapter.getItemCount() == 0 ? View.VISIBLE : View.GONE
+        );
+        transfersAdapter.endBatchedUpdates();
     }
 
 }
