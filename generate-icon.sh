30f4659267403c399275ab0e4442518d03325ced
David Piggott <david@piggott.me.uk>
2015-08-17 18:24:35 +0100

    Add custom icon

---
+++ generate-icon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+inkscape -z -e $DIR/app/src/main/res/mipmap-mdpi/ic_launcher.png -w 48 -h 48 icon.svg
+inkscape -z -e $DIR/app/src/main/res/mipmap-hdpi/ic_launcher.png -w 72 -h 72 icon.svg
+inkscape -z -e $DIR/app/src/main/res/mipmap-xhdpi/ic_launcher.png -w 96 -h 96 icon.svg
+inkscape -z -e $DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png -w 144 -h 144 icon.svg
