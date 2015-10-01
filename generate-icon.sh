7c39c5a2b920f354f818e202ef5fadebedde7320
David Piggott <david@piggott.me.uk>
2015-10-01 10:48:46 +0100

    Add xxxhdpi launcher icon

--- generate-icon.sh
+++ generate-icon.sh
@@ -7,4 +7,5 @@ inkscape -z -e $DIR/app/src/main/res/mipmap-mdpi/ic_launcher.png -w 48 -h 48 ico
 inkscape -z -e $DIR/app/src/main/res/mipmap-hdpi/ic_launcher.png -w 72 -h 72 icon.svg
 inkscape -z -e $DIR/app/src/main/res/mipmap-xhdpi/ic_launcher.png -w 96 -h 96 icon.svg
 inkscape -z -e $DIR/app/src/main/res/mipmap-xxhdpi/ic_launcher.png -w 144 -h 144 icon.svg
+inkscape -z -e $DIR/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png -w 192 -h 192 icon.svg
 inkscape -z -e $DIR/icon.png -w 512 -h 512 icon.svg
