1f8378da8f82dc7c0d783da03b1fa968814c39ae
David Piggott <david@piggott.me.uk>
2016-08-03 23:02:50 +0100

    Update screenshots

--- graphics/generate-feature.sh
+++ graphics/generate-feature.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-inkscape -z -e $DIR/feature.png -w 1024 -h 500 $DIR/feature.svg
+
+inkscape -z \
+    -e $DIR/feature.png \
+    -w 1024 \
+    -h 500 \
+    $DIR/feature.svg
