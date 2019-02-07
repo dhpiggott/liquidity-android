c59e7799d4d4d9d1bde16a6a9ff0e42090eab43b
David Piggott <david@piggott.me.uk>
2019-02-07 22:35:41 +0000

    Simplify getRelativeTimeSpanString
    Inline the main body and in doing so allow static analysis to show that
    abbrevRelative was false, hence the removal of it and the paths it would
    have led to if true.

--- app/src/main/java/com/dhpcs/liquidity/LiquidityApplication.kt
+++ app/src/main/java/com/dhpcs/liquidity/LiquidityApplication.kt
@@ -15,14 +15,74 @@ class LiquidityApplication : Application() {
 
     companion object {
 
+        @SuppressLint("PrivateResource")
         fun getRelativeTimeSpanString(context: Context,
                                       time: ReadableInstant,
                                       now: ReadableInstant,
                                       minResolution: Long): String {
-            val flags = (android.text.format.DateUtils.FORMAT_SHOW_DATE or
-                    android.text.format.DateUtils.FORMAT_SHOW_YEAR or
-                    android.text.format.DateUtils.FORMAT_ABBREV_MONTH)
-            return getRelativeTimeSpanString(context, time, now, minResolution, flags)
+            val timeRounded = DateTime(time).withMillisOfSecond(0)
+            val nowRounded = DateTime(now).withMillisOfSecond(0)
+            val past = !nowRounded.isBefore(timeRounded)
+            val interval = if (past) {
+                Interval(timeRounded, nowRounded)
+            } else {
+                Interval(nowRounded, timeRounded)
+            }
+
+            val count: Int
+            val resId: Int
+            if (minResolution < android.text.format.DateUtils.MINUTE_IN_MILLIS &&
+                    Minutes.minutesIn(interval).isLessThan(Minutes.ONE)) {
+                count = Seconds.secondsIn(interval).seconds
+                resId = if (past) {
+                    R.plurals.joda_time_android_num_seconds_ago
+                } else {
+                    R.plurals.joda_time_android_in_num_seconds
+                }
+            } else if (minResolution < android.text.format.DateUtils.HOUR_IN_MILLIS &&
+                    Hours.hoursIn(interval).isLessThan(Hours.ONE)) {
+                count = Minutes.minutesIn(interval).minutes
+                resId = if (past) {
+                    R.plurals.joda_time_android_num_minutes_ago
+                } else {
+                    R.plurals.joda_time_android_in_num_minutes
+                }
+            } else if (minResolution < android.text.format.DateUtils.DAY_IN_MILLIS &&
+                    Days.daysIn(interval).isLessThan(Days.ONE)) {
+                count = Hours.hoursIn(interval).hours
+                resId = if (past) {
+                    R.plurals.joda_time_android_num_hours_ago
+                } else {
+                    R.plurals.joda_time_android_in_num_hours
+                }
+            } else if (minResolution < android.text.format.DateUtils.WEEK_IN_MILLIS &&
+                    (if (past) {
+                        Weeks.weeksBetween(timeRounded.toLocalDate(), nowRounded.toLocalDate())
+                    } else {
+                        Weeks.weeksBetween(nowRounded.toLocalDate(), timeRounded.toLocalDate())
+                    }).isLessThan(Weeks.ONE)) {
+                count = (if (past) {
+                    Days.daysBetween(timeRounded.toLocalDate(), nowRounded.toLocalDate())
+                } else {
+                    Days.daysBetween(nowRounded.toLocalDate(), timeRounded.toLocalDate())
+                }.days)
+                resId = if (past) {
+                    R.plurals.joda_time_android_num_days_ago
+                } else {
+                    R.plurals.joda_time_android_in_num_days
+                }
+            } else {
+                return net.danlew.android.joda.DateUtils.formatDateRange(
+                        context,
+                        time,
+                        time,
+                        android.text.format.DateUtils.FORMAT_SHOW_DATE or
+                                android.text.format.DateUtils.FORMAT_SHOW_YEAR or
+                                android.text.format.DateUtils.FORMAT_ABBREV_MONTH
+                )
+            }
+
+            return String.format(context.resources.getQuantityString(resId, count), count)
         }
 
         fun formatCurrency(context: Context, currencyCode: String?): String {
@@ -160,106 +220,6 @@ class LiquidityApplication : Application() {
                 }
 
             }
-        }
-
-        @SuppressLint("PrivateResource")
-        private fun getRelativeTimeSpanString(context: Context,
-                                              time: ReadableInstant,
-                                              now: ReadableInstant,
-                                              minResolution: Long,
-                                              flags: Int): String {
-            val abbrevRelative = flags and (android.text.format.DateUtils.FORMAT_ABBREV_RELATIVE or
-                    android.text.format.DateUtils.FORMAT_ABBREV_ALL) != 0
-
-            val timeRounded = DateTime(time).withMillisOfSecond(0)
-            val nowRounded = DateTime(now).withMillisOfSecond(0)
-            val past = !nowRounded.isBefore(timeRounded)
-            val interval = if (past) {
-                Interval(timeRounded, nowRounded)
-            } else {
-                Interval(nowRounded, timeRounded)
-            }
-
-            val count: Long
-            val resId: Int
-            if (minResolution < android.text.format.DateUtils.MINUTE_IN_MILLIS &&
-                    Minutes.minutesIn(interval).isLessThan(Minutes.ONE)) {
-                count = Seconds.secondsIn(interval).seconds.toLong()
-                resId = if (past) {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_num_seconds_ago
-                    } else {
-                        R.plurals.joda_time_android_num_seconds_ago
-                    }
-                } else {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_in_num_seconds
-                    } else {
-                        R.plurals.joda_time_android_in_num_seconds
-                    }
-                }
-            } else if (minResolution < android.text.format.DateUtils.HOUR_IN_MILLIS &&
-                    Hours.hoursIn(interval).isLessThan(Hours.ONE)) {
-                count = Minutes.minutesIn(interval).minutes.toLong()
-                resId = if (past) {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_num_minutes_ago
-                    } else {
-                        R.plurals.joda_time_android_num_minutes_ago
-                    }
-                } else {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_in_num_minutes
-                    } else {
-                        R.plurals.joda_time_android_in_num_minutes
-                    }
-                }
-            } else if (minResolution < android.text.format.DateUtils.DAY_IN_MILLIS &&
-                    Days.daysIn(interval).isLessThan(Days.ONE)) {
-                count = Hours.hoursIn(interval).hours.toLong()
-                resId = if (past) {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_num_hours_ago
-                    } else {
-                        R.plurals.joda_time_android_num_hours_ago
-                    }
-                } else {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_in_num_hours
-                    } else {
-                        R.plurals.joda_time_android_in_num_hours
-                    }
-                }
-            } else if (minResolution < android.text.format.DateUtils.WEEK_IN_MILLIS &&
-                    (if (past) {
-                        Weeks.weeksBetween(timeRounded.toLocalDate(), nowRounded.toLocalDate())
-                    } else {
-                        Weeks.weeksBetween(nowRounded.toLocalDate(), timeRounded.toLocalDate())
-                    }).isLessThan(Weeks.ONE)) {
-                count = (if (past) {
-                    Days.daysBetween(timeRounded.toLocalDate(), nowRounded.toLocalDate())
-                } else {
-                    Days.daysBetween(nowRounded.toLocalDate(), timeRounded.toLocalDate())
-                }.days).toLong()
-                resId = if (past) {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_num_days_ago
-                    } else {
-                        R.plurals.joda_time_android_num_days_ago
-                    }
-                } else {
-                    if (abbrevRelative) {
-                        R.plurals.joda_time_android_abbrev_in_num_days
-                    } else {
-                        R.plurals.joda_time_android_in_num_days
-                    }
-                }
-            } else {
-                return net.danlew.android.joda.DateUtils.formatDateRange(context, time, time, flags)
-            }
-
-            val format = context.resources.getQuantityString(resId, count.toInt())
-            return String.format(format, count)
         }
 
     }
